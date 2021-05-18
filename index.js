770dd766eb5442270b38122e7dfe9f9b31b444b8
Luke <gianluca.suriani@outlook.it>
2021-05-18 09:27:43 +0200

    Updated JS / improved image preloading

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let pictures = [ 
+// var fs = require('fs');
+
+let defaultPictures = [ 
     ["aleskrivec",'Photo by <a href="https://unsplash.com/@aleskrivec?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ales Krivec</a> on <a href="https://unsplash.com/s/photos/landscape?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>'],
     ["kencheung",'Photo by <a href="https://unsplash.com/@kencheungphoto?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ken Cheung</a> on <a href="https://unsplash.com/s/photos/landscape?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>'],
     ["baileyzinde",'Photo by <a href="https://unsplash.com/@baileyzindel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Bailey Zindel</a> on <a href="https://unsplash.com/s/photos/landscape?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>'],
@@ -10,26 +12,22 @@ function getOrdinalNum(n) {
     return n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '');
 }
 
-
-tabNameTime()
-function tabNameTime() {
-    var Symbol;
+function TickLoop() {
     var today = new Date();
     var hours = today.getHours()
     var minutes = String(today.getMinutes()).padStart(2, "0");
 
     const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
-    var fulldate = monthNames[today.getMonth()] + " " +  getOrdinalNum(today.getDay()) + ", " + today.getFullYear()
+    var fulldate = monthNames[today.getMonth()] + " " +  getOrdinalNum(today.getDate()) + ", " + today.getFullYear()
     
     document.title = "New Tab | " + hours + ":" + minutes;
     document.getElementById("hours").innerText = hours;
     document.getElementById("minutes").innerText = minutes;
     document.getElementById("date").innerText = fulldate
     
-    setTimeout(tabNameTime, 1000);
+    setTimeout(TickLoop, 1000);
 }
-
-
+TickLoop()
 
 $('input[type="text"]').on('focus', function() {
     document.getElementById("bg").className = "background-hvr"
@@ -39,25 +37,33 @@ $('input[type="text"]').on('focusout', function() {
     document.getElementById("bg").className = "background"
 });
 
-
-
 document.onkeypress = function (e) {
     document.getElementById("search").focus();
 }
 
+// this needs to be implemented if i'll eventually add a custom pictures feature
+// and if anyone is worndering, i'm using browserify, it lets me use npm packages on plain javascript
+
+// const customFiles = '/assets/images/custom';
+// fs.readdir(testFolder, (err, files) => {
+//   files.forEach(file => {
+//     console.log(file);
+//   });
+// })
 
 
-
-var randomBg = pictures[Math.random() * pictures.length | 0]
+var randomBg = defaultPictures[Math.random() * defaultPictures.length | 0]
 var bgimage = new Image();      
-console.log(randomBg)
-console.log(randomBg[0])
-console.log(randomBg[1])
-bgimage.src="./assets/wallpapers/1080/" + randomBg[0] + ".jpg"; 
+bgimage.src="./assets/wallpapers/default/" + randomBg[0] + ".jpg"; 
 
-$(".credits").html("<img src=\"assets/logo/us_logo.png\" height=\"10px\" width=\"10px\"></img>" + " " + randomBg[1])     
+$(".credits").html("<img src=\"assets/icons/us_logo.png\" height=\"10px\" width=\"10px\"></img>" + " " + randomBg[1])     
 
 
-$(bgimage).on('load', function(){   
-    $(".background").css("background-image","url("+$(this).attr("src")+")").hide().fadeIn('slow')
-})
+// this doesn't work, why?
+
+
+$(document).ready(function() {
+    $(bgimage).on('load', function(){   
+        $(".background").css("background-image","url("+$(this).attr("src")+")").hide().fadeIn('slow')
+    })  
+});
